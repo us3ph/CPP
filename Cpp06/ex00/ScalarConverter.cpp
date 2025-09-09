912fb2793c341a0b273305d558983a1a68310c54
bornIIcode <josephtabia1@gmail.com>
2025-09-09 22:13:30 +0100

    IEEE 754

--- Cpp06/ex00/ScalarConverter.cpp
+++ Cpp06/ex00/ScalarConverter.cpp
@@ -30,22 +30,106 @@ bool ScalarConverter::isDouble(const std::string &literal)
 }
 int ScalarConverter::stringToInt(const std::string &literal)
 {
-
+    int num = 0;
+    std::stringstream ss(literal); // class from the <sstream>
+    ss >> num;
+    if(ss.fail() || !ss.eof())
+    {
+        std::cerr << "Overflow or invalid input";
+        return 0;
+    }
+    return num;
 }
 char ScalarConverter::stringToChar(const std::string &literal)
 {
-    return literal[1];
+    return literal[0];
 }
 float ScalarConverter::stringToFloat(const std::string &literal)
 {
-
+    float num = 0;
+    std::stringstream ss(literal);
+    ss >> num;
+    if(ss.fail() || !ss.eof()) // leftover character check (12bbfg)
+    {
+        std::cerr << "Overflow or invalid input";
+        return 0;
+    }
+    return num;
 }
 double ScalarConverter::stringToDouble(const std::string &literal)
 {
+    double num = 0;
+    std::stringstream ss(literal);
+    ss >> num;
+    if(ss.fail() || !ss.eof())
+    {
+        std::cerr << "Overflow or invalid input";
+        return 0;
+    }
+    return num;
+}
+
+void ScalarConverter::printChar(char value, bool impossible, bool Nondisplayable)
+{
+    if(impossible)
+        std::cout << "char: impossible" << std::endl;
+    else if(Nondisplayable)
+        std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char: '" << value << "'" << std::endl;
+}
+void ScalarConverter::printInt(int value, bool impossible)
+{
+    if(impossible)
+        std::cout<< "int: impossible" << std::endl;
+    std::cout << "int: '" << value << "'" << std::endl;
+}
+
+
+// IEEE 754 is a technical standard for floating-point arithmetic originally established in 1985 by the Institute of Electrical and Electronics Engineers (IEEE).
+void ScalarConverter::printFloat(float value)
+{
+    // in floating-point arithmetic(IEEE 754) dividing zero by zero produces a special value called NaN (0.0f / 0.0f)
+    if(value != value) // By IEEE 754 rules, NaN is the only floating-point value where x != x is true
+    {
+        std::cout << "float: nanf" << std::endl;
+        return;
+    }
+    else if(value > 1e38f)
+    {
+        std::cout << "float: +inff" << std::endl;
+        return;
+    } 
+    else if(value < -1e38f)
+    {
+        std::cout << "float: -inff" << std::endl;
+        return;
+    }
+    if(value = static_cast<int>(value)) // check normal number if decimal part = 0 is true like 42.0 
+        std::cout << "float: " << value << "0f" << std::endl;
+    else
+        std::cout << "float: " << value << "f" << std::endl; 
+}
+void ScalarConverter::printDouble(double value)
+{
+    if(value != value)
+        std::cout << "double: nan" << std::endl;
+    else if(value > 1e308)
+    {
+        std::
+        return;
+    }
+    else if(value < -1e308)
+    {
+        std::
+        return;
+    }
+    if()
     
 }
 
+
 void ScalarConverter::convert(const std::string& literal)
 {
-
+    if()
 }
--- Cpp06/ex00/ScalarConverter.hpp
+++ Cpp06/ex00/ScalarConverter.hpp
@@ -3,6 +3,7 @@
 
 #include "cmath"
 #include "iostream"
+#include "sstream"
 
 class ScalarConverter // static class 
 {
@@ -12,15 +13,21 @@ private:
     ScalarConverter& operator=(const ScalarConverter &other);
     ~ScalarConverter();
 
-    bool isInt(const std::string &literal);
-    bool isChar(const std::string &literal);
-    bool isFloat(const std::string &literal);
-    bool isDouble(const std::string &literal);
 
-    int stringToInt(const std::string &literal);
-    char stringToChar(const std::string &literal);
-    float stringToFloat(const std::string &literal);
-    double stringToDouble(const std::string &literal);
+    static bool isInt(const std::string &literal);
+    static bool isChar(const std::string &literal);
+    static bool isFloat(const std::string &literal);
+    static bool isDouble(const std::string &literal);
+
+    static int stringToInt(const std::string &literal);
+    static char stringToChar(const std::string &literal);
+    static float stringToFloat(const std::string &literal);
+    static double stringToDouble(const std::string &literal);
+
+    static void printChar(char value, bool impossible = false, bool Nondisplayable = false);
+    static void printInt(int value, bool impossible = false);
+    static void printFloat(float value);
+    static void printDouble(double value);
 
 public:
     static void convert(const std::string& literal);
